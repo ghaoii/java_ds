545dffd3d523cb5180b6798b6a3f69077039fba0
ghaoii <641517196@qq.com>
2022-04-17 15:25:25 +0800

    upload

---
+++ src/seqlist/MyArray.java
@@ -0,0 +1,181 @@
+package seqlist;
+
+import java.util.Arrays;
+
+public class MyArray {
+    private int[] data;
+    private int size = 0;
+
+    /**
+     * 向动态数组中添加元素
+     */
+    //向数组的末尾增加元素
+    public boolean add(int val){
+        data[size] = val;
+        size++;
+        //如果数组满了，则扩容
+        if(size == data.length){
+            grow();
+        }
+        return true;
+    }
+
+    //向index索引处添加新元素
+    public boolean add(int index, int val){
+        if(index < 0 || index > size){
+            System.out.println("val is illegal");
+            return false;
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = val;
+        size++;
+
+        if(size == data.length){
+            grow();
+        }
+        return true;
+    }
+
+    //扩容
+    private void grow() {
+        this.data = Arrays.copyOf(data, data.length * 2);
+    }
+
+    /**
+     * 查找动态数组中的元素
+     */
+    //根据元素值查找数组中第一次出现的此元素，找到了返回索引，否则返回-1
+    public int getByValue(int val){
+        for (int i = 0; i < size; i++) {
+            if(data[i] == val){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //查找当前动态数组中含有val，有则返回true，否则返回false
+    public boolean contains(int val){
+        return getByValue(val) != -1;
+    }
+
+    //查找当前数组中，索引为index的元素,找到返回元素值，否则返回-1
+    public int getByIndex(int index){
+        if(index < 0 || index >= size){
+            System.out.println("index is illegal");
+            return -1;
+        }
+        return data[index];
+    }
+
+    /**
+     * 修改动态数组中的元素
+     */
+    //修改索引为index的元素，并返回修改前的值，否则返回-1
+    public int setByIndex(int index, int newVal){
+        if(index < 0 || index >= size){
+            System.out.println("index is illegal");
+            return -1;
+        }
+        int ret = data[index];
+        data[index] = newVal;
+        return ret;
+    }
+
+    //修改动态数组中第一个元素值为oldVal的元素，修改成功，返回true，否则返回false
+    public boolean setByVal(int oldVal, int newVal){
+        int index = getByValue(oldVal);
+        if(index == -1){
+            System.out.println("oldVal is not exist");
+            return false;
+        }
+        data[index] = newVal;
+        return true;
+    }
+
+    /**
+     * 删除动态数组中的元素
+     */
+    //删除当前动态数组中索引为index的元素,删除成功返回删除前的值，否则返回-1
+    public int remove(int index){
+        //若动态数组中没有元素返回-1
+        if (size == 0) {
+            System.out.println("seqlist is empty");
+            return -1;
+        }
+        if(index < 0 || index >= size){
+            System.out.println("index is illegal");
+            return -1;
+        }
+        int ret = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = 0;
+        size--;
+        return ret;
+    }
+
+    //删除动态数组的头元素，若动态数组为空，返回-1
+    public int removeFirst(){
+        int ret = data[0];
+        remove(0);
+        return ret;
+    }
+
+    //删除动态述责的尾元素,若动态数组为空，返回-1
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("seqlist is empty");
+            return -1;
+        }
+        int ret = data[size - 1];
+        remove(size - 1);
+        return ret;
+    }
+
+    //删除第一个元素值为val的元素，删除成功返回true，否则返回false
+    public boolean removeByValFirst(int val){
+        int index = getByValue(val);
+        if(index == -1){
+            System.out.println("value is not exist");
+            return false;
+        }
+       return remove(index) != -1;
+    }
+
+    //删除所有元素值为val的元素值，删除成功返回true，否则返回false
+    public boolean removeByValAll(int val){
+        boolean ret = false;
+        for (int i = 0; i < size; i++) {
+            if(data[i] == val){
+                ret = true;
+                remove(i);
+            }
+        }
+        return ret;
+    }
+
+    public MyArray(){
+        this(10);
+    }
+
+    public MyArray(int initCap){
+        this.data = new int[initCap];
+    }
+
+    public String toString(){
+        String ret = "[";
+        for (int i = 0; i < size; i++) {
+            ret += data[i];
+            if(i < size - 1){
+                ret += ", ";
+            }
+        }
+        ret += "]";
+        return ret;
+    }
+
+}
---
+++ src/seqlist/Test.java
@@ -0,0 +1,18 @@
+package seqlist;
+
+import seqlist.MyArray;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray(3);
+        myArray.add(10);
+        myArray.add(20);
+        myArray.add(30);
+        myArray.add(20);
+        myArray.add(40);
+        System.out.println(myArray.removeByValAll(20));
+        System.out.println(myArray);
+    }
+}
