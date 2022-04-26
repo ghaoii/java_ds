7001429e56f23e414d4cef5567fd56c12e49a7f0
ghaoii <641517196@qq.com>
2022-04-26 18:09:14 +0800

    Num92_ReverseBetween的递归解法

--- src/seqlist/leetcode/Num92_ReverseBetween.java
+++ src/seqlist/leetcode/Num92_ReverseBetween.java
@@ -48,24 +48,51 @@ public class Num92_ReverseBetween {
 //        }
 //    }
 
-    //头插法
+    //迭代头插法
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        ListNode dummyHead = new ListNode();
+//        dummyHead.next = head;
+//        ListNode prev = dummyHead;
+//        ListNode cur = prev.next;
+//        for (int i = 0; i < left - 1; i++) {
+//            prev = prev.next;
+//            cur = cur.next;
+//        }
+//        for (int i = 0; i < right - left; i++) {
+//            //先保存cur的后继结点
+//            ListNode next = cur.next;
+//            cur.next = next.next;//把next单独分离出来，准备头插
+//            //头插
+//            next.next = prev.next;
+//            prev.next = next;
+//        }
+//        return dummyHead.next;
+//    }
+
+    //递归
+    ListNode successor = null;//用来存储索引为right的结点的后继
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummyHead = new ListNode();
-        dummyHead.next = head;
-        ListNode prev = dummyHead;
-        ListNode cur = prev.next;
-        for (int i = 0; i < left - 1; i++) {
-            prev = prev.next;
-            cur = cur.next;
+        if(left == 1){
+            return reverseK(head, right);
         }
-        for (int i = 0; i < right - left; i++) {
-            //先保存cur的后继结点
-            ListNode next = cur.next;
-            cur.next = next.next;//把next单独分离出来，准备头插
-            //头插
-            next.next = prev.next;
-            prev.next = next;
-        }
-        return dummyHead.next;
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+
     }
+
+    //反转前K的结点
+    public ListNode reverseK(ListNode head, int k){
+        if(k == 1){
+            successor = head.next;
+            return head;
+        }
+
+        ListNode node = head.next;
+        ListNode newHead = reverseK(head.next, k-1);
+        node.next = head;
+        head.next = successor;
+        return newHead;
+    }
+
+
 }
